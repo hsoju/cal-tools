7b071d56dc87dec6b635a9d9d89c19398a898386
hsoju <78128765+hsoju@users.noreply.github.com>
2021-04-09 17:09:54 -0400

    fix: update mesh loader to handle separate uvs

--- cal_tools/loader/mesh.py
+++ cal_tools/loader/mesh.py
@@ -17,39 +17,37 @@ class CalMeshLoader(CalLoader):
         vertex_ids = unpack_chunk(cmf.read(12), 'i')
         return CalFace(vertex_ids)
 
-    def parse_binary_blend_vertex(self, cmf: mmap, id_: int) -> CalBlendVertex:
+    def parse_binary_blend_vertex(self, cmf: mmap, id_: int, uv_count: int) -> CalBlendVertex:
         position = unpack_chunk(cmf.read(12), 'f')
         normal = unpack_chunk(cmf.read(12), 'f')
-        uv = unpack_chunk(cmf.read(8), 'f')
+        uv = None
+        for _ in range(uv_count):
+            uv = unpack_chunk(cmf.read(8), 'f')
         return CalBlendVertex(id_, position, normal, uv)
 
-    def parse_binary_vertex(self, cmf: mmap) -> CalVertex:
+    def parse_binary_vertex(self, cmf: mmap, uv_count: int) -> CalVertex:
         position = unpack_chunk(cmf.read(12), 'f')
         normal = unpack_chunk(cmf.read(12), 'f')
         color = unpack_chunk(cmf.read(12), 'f')
         cmf.seek(8, os.SEEK_CUR)
-        uv = unpack_chunk(cmf.read(8), 'f')
+        uv = None
+        for _ in range(uv_count):
+            uv = unpack_chunk(cmf.read(8), 'f')
         influence_count = struct.unpack('i', cmf.read(4))[0]
         influences = []
         for _ in range(influence_count):
             infl_id, infl_weight = struct.unpack('i', cmf.read(4))[0], struct.unpack('f', cmf.read(4))[0]
             influences.append((infl_id, infl_weight))
-        # print(position)
-        # print(normal)
-        # print(color)
-        # print(uv)
-        # print(influences)
         return CalVertex(position, normal, uv, color, influences)
 
-    def parse_morph(self, cmf: mmap, vertex_count: int) -> CalMorph:
+    def parse_morph(self, cmf: mmap, vertex_count: int, uv_count: int) -> CalMorph:
         morph_name_length = struct.unpack('i', cmf.read(4))[0]
         morph_name = cmf.read(morph_name_length - 1).decode('utf-8')
         cmf.seek(1, os.SEEK_CUR)
-        # print(morph_name)
         blend_vertices = []
         next_blend_vertex_id = struct.unpack('i', cmf.read(4))[0]
         while next_blend_vertex_id < vertex_count:
-            blend_vertices.append(self.parse_binary_blend_vertex(cmf, next_blend_vertex_id))
+            blend_vertices.append(self.parse_binary_blend_vertex(cmf, next_blend_vertex_id, uv_count))
             next_blend_vertex_id = struct.unpack('i', cmf.read(4))[0]
         return CalMorph(morph_name, blend_vertices)
 
@@ -57,20 +55,17 @@ class CalMeshLoader(CalLoader):
         material = struct.unpack('i', cmf.read(4))[0]
         vertex_count = struct.unpack('i', cmf.read(4))[0]
         face_count = struct.unpack('i', cmf.read(4))[0]
-        cmf.seek(12, os.SEEK_CUR)
+        cmf.seek(8, os.SEEK_CUR)
+        uv_count = struct.unpack("i", cmf.read(4))[0]
         morph_count = struct.unpack("i", cmf.read(4))[0]
-        # for _ in range(10):
-        #     read = cmf.read(4)
-        #     print(f'{struct.unpack("i", read)} {struct.unpack("f", read)}')
-        # exit()
         vertices = []
         for _ in range(vertex_count):
-            vertices.append(self.parse_binary_vertex(cmf))
+            vertices.append(self.parse_binary_vertex(cmf, uv_count))
         morphs = None
         if morph_count > 0:
             morphs = []
             for _ in range(morph_count):
-                morphs.append(self.parse_morph(cmf, vertex_count))
+                morphs.append(self.parse_morph(cmf, vertex_count, uv_count))
         faces = []
         for _ in range(face_count):
             faces.append(self.parse_binary_face(cmf))
@@ -83,13 +78,6 @@ class CalMeshLoader(CalLoader):
             submeshes = []
             for _ in range(submesh_count):
                 submeshes.append(self.parse_binary_submesh(cmf))
-            # self.parse_binary_submesh(cmf)
-            # self.parse_binary_submesh(cmf)
-            # cmf.seek(16, os.SEEK_CUR)
-            # self.parse_binary_submesh(cmf)
-            # for _ in range(10):
-            #     read = cmf.read(4)
-            #     print(f'{struct.unpack("i", read)} {struct.unpack("f", read)}')
         return CalMesh(submeshes)
 
     def load_ascii(self) -> CAL_OBJECT:
@@ -98,5 +86,6 @@ class CalMeshLoader(CalLoader):
 
 shirtpath = "C:/Users/JM729/Desktop/Lynx/Gaming/Virtual/iMVu/Testing/Clothing/Shirt-Encrypted.xmf"
 fpath = "C:/Users/JM729/Desktop/Lynx/Gaming/Virtual/iMVu/Testing/Accessories/Wings/Wings-Encrypted.xmf"
-cc = CalMeshLoader(shirtpath)
+headpath = "C:/Users/JM729/Desktop/Lynx/Gaming/Virtual/iMVu/Testing/Heads/F-Encrypted.xmf"
+cc = CalMeshLoader(headpath)
 cc.load()
